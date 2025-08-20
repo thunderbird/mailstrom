dcd9dba0c8dc81de46ab6cf1260be7bb32e6b4cd
Melissa Autumn <melissa@thunderbird.net>
2025-08-20 12:44:52 -0700

    Add OIDC/Keycloak configuration and secrets in stalwart toml (Fixes #79)

--- pulumi/bootstrap/bootstrap.py
+++ pulumi/bootstrap/bootstrap.py
@@ -79,6 +79,7 @@ def get_secrets(env: str, aws_region: str) -> dict:
         's3_iam_access_key': f'mailstrom/{env}/iam.user.mailstrom-{env}-stalwart.access_key',
         's3_backend': f'mailstrom/{env}/stalwart.postboot.s3_backend',
         'tb_accounts_backend': f'mailstrom/{env}/stalwart.postboot.tb-accounts_backend',
+        'keyclock_backend': f'mailstrom/{env}/stalwart.postboot.keycloak_backend',
     }
     sm_client = boto3.client('secretsmanager', region_name=aws_region)
 
