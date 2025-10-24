1122af81e75a462e37d35613fd2cab246cb4085c
Ryan Jung <rjung@thunderbird.net>
2025-10-24 09:03:43 -0600

    Rewire Redis code

--- pulumi/stalwart/redis.py
+++ pulumi/stalwart/redis.py
@@ -8,21 +8,14 @@ import tb_pulumi.secrets
 
 
 def redis(
-    self, cache_node_type: str, cache_node_count: int, cache_parameters: list
+    self, redis_opts: dict,
 ) -> tuple[tb_pulumi.elasticache.ElastiCacheReplicationGroup, tb_pulumi.secrets.SecretsManagerSecret]:
     """Build Redis resources.
 
-    :param cache_node_count: Number of Redis cluster nodes to build. This must be at least 1. When greater than 1, one
-        primary "write" node will be created with (n - 1) read-only replicas. Defaults to 1.
-    :type cache_node_count: int, optional
-
-    :param cache_node_type: The `ElastiCache instance type
-        <https://docs.aws.amazon.com/AmazonElastiCache/latest/dg/CacheNodes.SupportedTypes.html>`_ to use when building
-        Redis cache nodes. Defaults to "cache.t3.micro".
-    :type cache_node_type: str, optional
-
-    :param cache_parameters: Dictionary of parameters in the parameter group to override.
-    :type cache_parameters: dict, optional
+    :param redis_opts: Additional `ReplicationGroup
+        <https://www.pulumi.com/registry/packages/aws/api-docs/elasticache/replicationgroup/#inputs>`_ inputs to pass
+        into the Elasticache constructor.
+    :type redis_opts: dict, optional
 
     :return: Tuple containing an ElastiCache Redis group and a Secrets Manager secret with the connection URL.
     :rtype: tuple[tb_pulumi.elasticache.ElastiCacheReplicationGroup, tb_pulumi.secrets.SecretsManagerSecret]
@@ -32,17 +25,11 @@ def redis(
         name=f'{self.name}-redis',
         project=self.project,
         subnets=self.subnets,
-        description=f'Redis cluster for {self.name}',
-        engine='redis',
-        engine_version='7.1',
-        node_type=cache_node_type,
-        num_cache_nodes=cache_node_count,
-        parameter_group_family='redis7',
-        parameter_group_params=cache_parameters,
         source_cidrs=[],
         source_sgids=[self.node_sgs[sg].resources['sg'].id for sg in self.node_sgs],
         opts=pulumi.ResourceOptions(parent=self, depends_on=[*self.node_sgs.values()]),
         tags=self.tags,
+        **redis_opts,
     )
 
     # Store Redis config details in Secrets Manager
