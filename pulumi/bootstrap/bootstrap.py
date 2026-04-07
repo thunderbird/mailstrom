da7db96a8a6cfa530885fafc4cb0015c27f876b2
Ryan Jung <rjung@thunderbird.net>
2026-04-07 14:26:06 -0600

    Rebuild dev, add env to templates

--- pulumi/bootstrap/bootstrap.py
+++ pulumi/bootstrap/bootstrap.py
@@ -19,6 +19,7 @@ TEMPLATE_MAP = {
 }
 # Map of template variable to EC2 tags
 TEMPLATE_VALUE_TAG_MAP = {
+    'env': 'environment',
     'function': 'postboot.stalwart.function',
     'https_paths': 'postboot.stalwart.https_paths',
     'node_services': 'postboot.stalwart.node_services',
